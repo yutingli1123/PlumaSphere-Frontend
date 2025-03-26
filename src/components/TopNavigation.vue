abd95df7181a914f56528d21eb4ca210938599e4
Yuting Li <yutingli1123@outlook.com>
2025-03-26 23:03:41 +0800

    Refactor App layout and remove unused components
    - Replaced HelloWorld component with TopNavigation in App.vue.
    - Updated layout structure to use el-header and el-main.
    - Removed unused components: HelloWorld, TheWelcome, WelcomeItem, and various icons.
    - Deleted base.css and main.css as they were no longer needed.
    - Added AlimamaShuHeiTi-Bold font file for TopNavigation styling.
    - Cleaned up router by removing the about route.

---
+++ src/components/TopNavigation.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <el-row class="top-nav" justify="space-between">
+    <RouterLink to="/" class="blog-title">PlumaSphere</RouterLink>
+  </el-row>
+</template>
+
+<style scoped>
+@font-face {
+  font-family: 'AlimamaShuHeiTi';
+  src: url('@/assets/fonts/AlimamaShuHeiTi-Bold.woff2') format('woff2');
+  font-weight: bold;
+  font-style: normal;
+}
+
+.top-nav {
+  background: linear-gradient(to left, #bfbfbf, #8c8c8c);
+  height: 150px;
+  align-items: end;
+}
+
+.blog-title {
+  align-self: center;
+  margin-left: 60px;
+  font-size: 48px;
+  font-weight: bold;
+  font-family: 'AlimamaShuHeiTi', sans-serif;
+  color: #333333;
+  text-decoration: none;
+}
+</style>
