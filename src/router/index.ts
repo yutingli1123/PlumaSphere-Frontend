4d7c142379bfaee94f5209ed72a2d719ac7b0ec6
Yuting Li <yutingli1123@outlook.com>
2025-03-30 09:50:16 +0800

    Add empty ArticleView component and update router configuration

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ArticleView from '@/views/ArticleView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,12 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
+    {
+      path: '/articles/:articleId',
+      name: 'article',
+      component: ArticleView,
+      props: true,
+    }
   ],
 })
 
