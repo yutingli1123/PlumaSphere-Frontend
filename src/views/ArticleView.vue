2aff557a1bba911757c8d0f05dacf9ad67b5531f
Yuting Li <yutingli1123@outlook.com>
2025-03-30 18:18:33 +0800

    Enhance global styles and integrate TopNavigation in ArticleView

--- src/views/ArticleView.vue
+++ src/views/ArticleView.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
+import TopNavigation from '@/components/TopNavigation.vue'
+
 defineProps({
-  articleId: String
+  articleId: String,
 })
 </script>
 
 <template>
-
+  <el-container>
+    <el-header style="padding: 0;">
+      <TopNavigation />
+    </el-header>
+  </el-container>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
