0d0c397bc98e8a0c6d36b44d638f85d36d12eaab
Yuting Li <yutingli1123@outlook.com>
2025-04-27 18:18:16 +0800

    Add Axios instance with interceptors for API handling and install Axios dependency.

---
+++ src/utils/axios.ts
@@ -0,0 +1,59 @@
+import type { AxiosInstance } from 'axios'
+import axios, { type AxiosError } from 'axios'
+import { ElMessage } from 'element-plus'
+import { useAuthStore } from '@/stores/auth.ts'
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}`,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    const authStore = useAuthStore()
+    const token = await authStore.getAccessToken
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+
+    return config
+  },
+  (error) => Promise.reject(error),
+)
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response.data
+  },
+  (error: AxiosError) => {
+    if (error.response) {
+      switch (error.response.status) {
+        case 401:
+          ElMessage.error('Unauthorized')
+          break
+        case 403:
+          ElMessage.error('Forbidden')
+          break
+        case 404:
+          ElMessage.error('Not Found')
+          break
+        case 500:
+          ElMessage.error('Internal Server Error')
+          break
+        default:
+          ElMessage.error('Something went wrong')
+      }
+    } else if (error.request) {
+      ElMessage.error('Network Error')
+    } else {
+      ElMessage.error('Something went wrong')
+    }
+
+    return Promise.reject(error)
+  },
+)
+
+export default axiosInstance
