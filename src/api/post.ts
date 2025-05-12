2dc840f1657f0c65a2e5a453c9aa11533d85769f
Yuting Li <yutingli1123@outlook.com>
2025-05-12 22:35:31 +0800

    Add endpoint for retrieving post count and update getAllPosts to support pagination

--- src/api/post.ts
+++ src/api/post.ts
@@ -3,8 +3,8 @@ import axiosInstance from '@/utils/axios.ts'
 import { ApiEndpoint, getPath } from '@/api/endpoints.ts'
 
 export const postApi = {
-  async getAllPosts(): Promise<Article[] | undefined> {
-    return await axiosInstance.get(getPath(ApiEndpoint.POST_GET_ALL))
+  async getAllPosts(page: number): Promise<Article[] | undefined> {
+    return await axiosInstance.get(`${getPath(ApiEndpoint.POST_GET_ALL)}?page=${page}`)
   },
   async getPostById(id: string): Promise<Article | undefined> {
     return await axiosInstance.get(getPath(ApiEndpoint.POST_GET_BY_ID, { id }))
@@ -13,5 +13,8 @@ export const postApi = {
     await axiosInstance.post(getPath(ApiEndpoint.POST_CREATE), post, {
       requiresAuth: true,
     })
+  },
+  async getPostsCount(): Promise<number> {
+    return await axiosInstance.get(getPath(ApiEndpoint.POST_GET_COUNT))
   },
 }
