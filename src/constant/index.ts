43b3cd424301213fe9a4197e91a1778ebad0ef13
Yuting Li <yutingli1123@outlook.com>
2025-05-20 22:18:05 +0800

    Add LIKE_COMMENT type to WebSocketMessageType enum for comment likes

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -3,4 +3,5 @@ export const tagTypes = ['primary', 'success', 'warning', 'danger'] as const
 export enum WebSocketMessageType {
   NEW_COMMENT = 'NEW_COMMENT',
   LIKE_POST = 'LIKE_POST',
+  LIKE_COMMENT = 'LIKE_COMMENT',
 }
