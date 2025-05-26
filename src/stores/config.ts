9e6f92f7efe0eff66eba545a6f4ea6c2f7a02fd6
Yuting Li <yutingli1123@outlook.com>
2025-05-26 22:41:11 +0800

    Optimize initialConfig method to prevent multiple concurrent executions

--- src/stores/config.ts
+++ src/stores/config.ts
@@ -6,30 +6,38 @@ import { ConfigFiled } from '@/constant'
 export const useConfigStore = defineStore('config', () => {
   const config = ref<Config[] | undefined>()
 
+  let initialPromise: Promise<void> | null = null
+
   const initialConfig = async () => {
-    const configData = localStorage.getItem('config')
-    const version = await systemApi.getStatusVersion()
-    if (configData) {
-      const parsedConfig = JSON.parse(configData) as Config[]
-      const configVersion = parsedConfig.find(
-        (item) => item.configKey === ConfigFiled.CONFIG_VERSION.toLowerCase(),
-      )
-      if (!version || (configVersion && configVersion.configValue === version)) {
-        config.value = parsedConfig.filter(
-          (item) => item.configKey !== ConfigFiled.CONFIG_VERSION.toLowerCase(),
+    if (initialPromise) {
+      return initialPromise
+    }
+    initialPromise = (async () => {
+      const configData = localStorage.getItem('config')
+      const version = await systemApi.getStatusVersion()
+      if (configData) {
+        const parsedConfig = JSON.parse(configData) as Config[]
+        const configVersion = parsedConfig.find(
+          (item) => item.configKey === ConfigFiled.CONFIG_VERSION.toLowerCase(),
         )
-        return
+        if (!version || (configVersion && configVersion.configValue === version)) {
+          config.value = parsedConfig.filter(
+            (item) => item.configKey !== ConfigFiled.CONFIG_VERSION.toLowerCase(),
+          )
+          return
+        }
       }
-    }
-    const newConfigData = await systemApi.getStatus()
-    if (newConfigData) {
-      config.value = newConfigData
-      newConfigData.push({
-        configKey: ConfigFiled.CONFIG_VERSION.toLowerCase(),
-        configValue: version,
-      } as Config)
-      localStorage.setItem('config', JSON.stringify(newConfigData))
-    }
+      const newConfigData = await systemApi.getStatus()
+      if (newConfigData) {
+        config.value = newConfigData
+        newConfigData.push({
+          configKey: ConfigFiled.CONFIG_VERSION.toLowerCase(),
+          configValue: version,
+        } as Config)
+        localStorage.setItem('config', JSON.stringify(newConfigData))
+      }
+    })()
+    await initialPromise
   }
 
   const resetConfig = () => {
